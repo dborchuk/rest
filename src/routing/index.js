e2f6ddef070b50d1332825e2039ba8b0f31c69de
Dmytro Borchuk <dborchuk@remit.se>
2018-01-26 11:24:10 +0200

    Added pug

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,6 +1,10 @@
 import posts from './posts';
 
 export default function (app) {
+  app.get('/', function (req, res) {
+    res.render('index');
+  });
+
   app.get('/api', function (req, res) {
     res.send('API is running');
   });
--- src/server.js
+++ src/server.js
@@ -14,14 +14,12 @@ export default function() {
 
   app.use(morgan('dev'));
   app.use(bodyParser.json())
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', './src/public')
+  app.set('view engine', 'pug');
 
   routing(app);
   migrations();
-
-  app.listen(PORT, () => {
-    log.info(`Express server listening on port ${PORT}`);
-  });
 
   app.use((req, res, next) => {
     res.status(404);
@@ -35,5 +33,9 @@ export default function() {
     log.error('Internal error(%d): %s', res.statusCode, err.message);
     res.send({ error: err.message });
     return;
+  });
+
+  app.listen(PORT, () => {
+    log.info(`Express server listening on port ${PORT}`);
   });
 }
